3f7206c5b638e5107ccbffdccc9f19e707b8af25
OrlandoSA <55679399+OrlandoSA@users.noreply.github.com>
2020-05-11 01:21:06 -0700

    continuacion de la lista de trabajos

--- app/src/main/java/beltran/miguel/a3ayudaempleado/PerfilActivity.kt
+++ app/src/main/java/beltran/miguel/a3ayudaempleado/PerfilActivity.kt
@@ -1,22 +1,31 @@
 package beltran.miguel.a3ayudaempleado
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.Toast
 import com.firebase.ui.auth.AuthUI
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
+import kotlinx.android.synthetic.main.activity_chat.*
 import kotlinx.android.synthetic.main.activity_perfil.*
+import kotlinx.android.synthetic.main.trabajos.view.*
 
 class PerfilActivity : AppCompatActivity() {
-
+    var listaTrabajos=ArrayList<Trabajos>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
 
         obtenerPerfil()
+        var adaptador=AdaptadorTrabajos(this, listaTrabajos)
+        lista_trabajos.adapter=adaptador
 
         btn_salir.setOnClickListener {
             cerrarSesion()
@@ -59,5 +68,45 @@ class PerfilActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         moveTaskToBack(true)
+    }
+    private class AdaptadorTrabajos: BaseAdapter {
+        var trabajo=ArrayList<Trabajos>()
+        var contexto: Context?=null
+
+        constructor(contexto: Context, trabajo:ArrayList<Trabajos>){
+            this.contexto=contexto
+            this.trabajo=trabajo
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var trabaj=trabajo[position]
+            var inflador= LayoutInflater.from(contexto)
+            var vista=inflador.inflate(R.layout.mensajes,null)
+
+            vista.trabajo_titulo.setText(trabaj.trabajo)
+            vista.trabajo_nombre.setText(trabaj.nombre)
+
+            vista.trabajo_select.setOnClickListener{
+                var intent = Intent(contexto, ChatActivity::class.java)
+                intent.putExtra("trabajo",trabaj.trabajo)
+                intent.putExtra("nombre",trabaj.nombre)
+                intent.putExtra("listaTrabajo",trabaj.listaMensaje)
+                contexto!!.startActivity(intent)
+            }
+
+            return vista
+        }
+
+        override fun getItem(position: Int): Any {
+            return trabajo[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return trabajo.size
+        }
     }
 }
